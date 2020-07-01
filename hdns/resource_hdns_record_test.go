c901ce7c552a9122ad70961439d47697dfc229a3
Alexey Remizov <alexey@remizov.org>
2020-07-01 09:47:05 +0300

    Added test case with multiple records

--- hdns/resource_hdns_record_test.go
+++ hdns/resource_hdns_record_test.go
@@ -29,6 +29,16 @@ func TestAccHdnsRecord_Basic(t *testing.T) {
 						"hdns_record.bar", "ttl", "86400"),
 				),
 			},
+			{
+				Config: testAccHdnsCheckRecordConfig_multiple_records(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccHdnsCheckRecordExists("hdns_record.bar", &record),
+					resource.TestCheckResourceAttr(
+						"hdns_record.bar", "name", "www-0"),
+					resource.TestCheckResourceAttr(
+						"hdns_record.bar", "ttl", "86400"),
+				),
+			},
 		},
 	})
 }
@@ -40,11 +50,29 @@ resource "hdns_zone" "foo" {
   name = "%s.dev"
 }
 
-resource "hdns_record" "bar" {
+resource "hdns_record" "foo" {
   name    = "www"
   type    = "A"
   value   = "1.1.1.1"
   zone_id = hdns_zone.foo.id 
+}
+`, rName)
+}
+
+//noinspection GoSnakeCaseUsage
+func testAccHdnsCheckRecordConfig_multiple_records(rName string) string {
+	return fmt.Sprintf(`
+resource "hdns_zone" "foo" {
+  name = "%s.dev"
+}
+
+resource "hdns_record" "bar" {
+  count = 10
+
+  name    = "www-${count.index}"
+  type    = "A"
+  value   = "1.1.1.${count.index}"
+  zone_id = hdns_zone.foo.id
 }
 `, rName)
 }
